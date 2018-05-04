dfc073fab0ced235f82db0594e855ea2d4aa77f4
Harkaran Brar <harkaranbrar7@gmail.com>
2018-05-03 18:40:46 -0700

    Revert "Merge"

--- TLoL/include/Enemy191T.h
+++ TLoL/include/Enemy191T.h
@@ -27,7 +27,7 @@ class Enemy191T
         double Xpos;
         double Ypos;
         double Zpos;
-        double destXPos, destYPos;
+
         double xVel, yVel;
 
         double getxVel();
@@ -54,9 +54,6 @@ class Enemy191T
     protected:
 
     private:
-        double scaleSizeX;
-        double scaleSizeY;
-        double scaleSizeZ;
 
 };
 
